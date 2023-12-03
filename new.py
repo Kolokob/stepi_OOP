29a2f0a46d180a882e87987d2f8ed51dde9320bb
Kolokob <114718232+Kolokob@users.noreply.github.com>
2023-12-02 16:40:32 -0800

    Update new.py
    uncommented some classes

--- new.py
+++ new.py
@@ -1,59 +1,59 @@
-# class AbstractClass:
-#     def __new__(cls, *args, **kwargs):
-#         return "Ошибка: нельзя создавать объекты абстрактного класса"
+class AbstractClass:
+    def __new__(cls, *args, **kwargs):
+        return "Ошибка: нельзя создавать объекты абстрактного класса"
 
 
-# class SingletonFive:
-#
-#     __instance = None
-#     count = 0
-#
-#     def __new__(cls, *args, **kwargs):
-#         if cls.count <= 5:
-#             cls.__instance = super().__new__(cls)
-#             cls.count += 1
-#         else:
-#             return cls.__instance
-#
-#     def __init__(self, *name):
-#         self.name = name
+class SingletonFive:
+
+    __instance = None
+    count = 0
+
+    def __new__(cls, *args, **kwargs):
+        if cls.count <= 5:
+            cls.__instance = super().__new__(cls)
+            cls.count += 1
+        else:
+            return cls.__instance
+
+    def __init__(self, *name):
+        self.name = name
 
 
 
 
-# TYPE_OS = 1
-#
-# class DialogWindows:
-#     name_class = "DialogWindows"
-#
-# class DialogLinux:
-#     name_class = "DialogLinux"
-#
-# class Dialog:
-#
-#     __instance = None
-#
-#     def __new__(cls, *args, **kwargs):
-#         if TYPE_OS == 1:
-#             cls.__instance = super().__new__(DialogLinux)
-#         else:
-#             cls.__instance = super().__new__(DialogWindows)
-#
-#         cls.__instance.name = args
-#         return cls.__instance
+TYPE_OS = 1
+
+class DialogWindows:
+    name_class = "DialogWindows"
+
+class DialogLinux:
+    name_class = "DialogLinux"
+
+class Dialog:
+
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if TYPE_OS == 1:
+            cls.__instance = super().__new__(DialogLinux)
+        else:
+            cls.__instance = super().__new__(DialogWindows)
+
+        cls.__instance.name = args
+        return cls.__instance
 
 
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#
-#     def clone(self):
-#         return Point(self.x, self.y)
-#
-#
-# pt = Point(3, 4)
-# pt_clone = pt.clone()
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def clone(self):
+        return Point(self.x, self.y)
+
+
+pt = Point(3, 4)
+pt_clone = pt.clone()
 
 
 
