74e7168c59883ae2f5d89dfc90993d5b6cdcdca1
Kolokob <114718232+Kolokob@users.noreply.github.com>
2023-12-02 16:37:15 -0800

    Update class_and_static_methonds.py
    uncommented class Factory

--- class_and_static_methonds.py
+++ class_and_static_methonds.py
@@ -1,27 +1,27 @@
 
-# class Factory:
-#     @staticmethod
-#     def build_sequence():
-#         empty_string = []
-#         return empty_string
-#
-#     @staticmethod
-#     def build_number(string):
-#         return float(string)
-#
-#
-#
-# class Loader:
-#     @staticmethod
-#     def parse_format(string, factory):
-#         seq = factory.build_sequence()
-#         for sub in string.split(","):
-#             item = factory.build_number(sub)
-#             seq.append(item)
-#
-#         return seq
-#
-# res = Loader.parse_format("1, 2, 3, -5, 10", Factory)
+class Factory:
+    @staticmethod
+    def build_sequence():
+        empty_string = []
+        return empty_string
+
+    @staticmethod
+    def build_number(string):
+        return float(string)
+
+
+
+class Loader:
+    @staticmethod
+    def parse_format(string, factory):
+        seq = factory.build_sequence()
+        for sub in string.split(","):
+            item = factory.build_number(sub)
+            seq.append(item)
+
+        return seq
+
+res = Loader.parse_format("1, 2, 3, -5, 10", Factory)
 
 
 
